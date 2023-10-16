3efae9bb5fd7b63f9a953f07fd5b30685be992c7
terumasa4649 <146915725+teru4649@users.noreply.github.com>
2023-10-16 21:26:42 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,8 @@
+//関数の定義
+const calculateSquare =  (value) => {
+  console.log('引数の値は： ' + value + 'です');
+  console.log('引数の2乗値は：' + value * value + 'です');
+}
+
+//関数を呼び出して、引数へ値を渡す
+calculateSquare(11);
